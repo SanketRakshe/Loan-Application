36baf47313e363989d90f6c1c276c34519c93871
SanketRakshe <sanketrakshe07@gmail.com>
2024-10-21 15:08:20 +0530

    FEAT: Seted up product service proxy

---
+++ src/main/java/com/example/Loan_Application/proxy/ProductServiceProxy.java
@@ -0,0 +1,13 @@
+package com.example.Loan_Application.proxy;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.Loan_Application.model.Product;
+
+@FeignClient(name="product-service", url="localhost:8000")
+public interface ProductServiceProxy {
+	@GetMapping("/product_info/{productName}")
+	public Product getProduct(@PathVariable String productName);
+}
